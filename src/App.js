0f90ea6318d642fc8b5f92f5e81161741b4f995b
lavanyaguru17 <109407476+lavanyaguru17@users.noreply.github.com>
2022-07-16 12:02:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Lavanya</h1>
+      <h2>Welcome to software development!!!</h2>
+    </div>
+  );
+}
