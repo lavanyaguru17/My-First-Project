e48b6f8f6d963fbc15f12d4991861a1344220b92
lavanyaguru17 <109407476+lavanyaguru17@users.noreply.github.com>
2022-07-16 12:27:28 +0530

    updated app file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Lavanya</h1>
+      <h1>Hello Lavanya!!!</h1>
       <h2>Welcome to software development!!!</h2>
+      <h3>Chidhambaram!!!</h3>
     </div>
   );
 }
